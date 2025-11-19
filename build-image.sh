609e11eb0255b47bec3fa0cf50652f9474a17096
Ubuntu <ubuntu@FVGKCPRNC01LSAW.fvg.sviluppo.dom>
2025-11-19 13:45:02 +0000

    added

---
+++ build-image.sh
@@ -0,0 +1,1 @@
+docker build -t mcp-crawl4ai-rag:local .
---
+++ insert_source.sql
@@ -0,0 +1,7 @@
+INSERT INTO sources (url, depth, active, created_at)
+VALUES (
+    'https://www.almaviva.it/it_IT/', 
+    2, 
+    true, 
+    NOW()
+);
